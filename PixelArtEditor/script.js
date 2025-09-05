7f7bfd5d8c2f718ae7c11ee15f545af3c330a222
Dumitriu Denis <denisdumitriu95@gmail.com>
2025-09-06 00:59:42 +0300

    Update script.js

--- PixelArtEditor/script.js
+++ PixelArtEditor/script.js
@@ -4,8 +4,14 @@ const colorPicker = document.getElementById('colorPicker');
 const clearButton = document.getElementById('clearButton');
 const saveButton = document.getElementById('saveButton');
 const gridSizeInput = document.getElementById('gridSize');
+const imageLoader = document.getElementById('imageLoader');
+const filterSelect = document.getElementById('filterSelect');
+const applyFilterButton = document.getElementById('applyFilterButton');
+
 let gridSize = parseInt(gridSizeInput.value);
 let drawing = false;
+
+// ---------------- PIXEL ART FUNCTIONS ----------------
 
 function drawGrid() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -29,6 +35,7 @@ function drawPixel(x, y) {
   ctx.fillRect(x, y, gridSize, gridSize);
 }
 
+// Mouse events
 canvas.addEventListener('mousedown', (e) => {
   drawing = true;
   const x = Math.floor(e.offsetX / gridSize) * gridSize;
@@ -46,11 +53,13 @@ canvas.addEventListener('mousemove', (e) => {
 
 canvas.addEventListener('mouseup', () => drawing = false);
 
+// Clear button
 clearButton.addEventListener('click', () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawGrid();
 });
 
+// Save button
 saveButton.addEventListener('click', () => {
   const dataURL = canvas.toDataURL();
   const link = document.createElement('a');
@@ -59,9 +68,66 @@ saveButton.addEventListener('click', () => {
   link.click();
 });
 
+// Grid size
 gridSizeInput.addEventListener('change', () => {
   gridSize = parseInt(gridSizeInput.value);
   drawGrid();
 });
 
 drawGrid();
+
+// ---------------- IMAGE & FILTER FUNCTIONS ----------------
+
+function loadAndProcessImage(file) {
+  const reader = new FileReader();
+  reader.onload = function(event) {
+    const img = new Image();
+    img.onload = function() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+    img.src = event.target.result;
+  };
+  reader.readAsDataURL(file);
+}
+
+function applyFilter(imageData, filterType) {
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const r = data[i];
+    const g = data[i + 1];
+    const b = data[i + 2];
+
+    if (filterType === 'sepia') {
+      data[i]     = Math.min(0.393*r + 0.769*g + 0.189*b, 255);
+      data[i + 1] = Math.min(0.349*r + 0.686*g + 0.168*b, 255);
+      data[i + 2] = Math.min(0.272*r + 0.534*g + 0.131*b, 255);
+    } else if (filterType === 'grayscale') {
+      const avg = (r + g + b) / 3;
+      data[i] = data[i+1] = data[i+2] = avg;
+    } else if (filterType === 'invert') {
+      data[i] = 255 - r;
+      data[i+1] = 255 - g;
+      data[i+2] = 255 - b;
+    }
+  }
+  return imageData;
+}
+
+function applyFilterToCanvas(filterType) {
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const filteredData = applyFilter(imageData, filterType);
+  ctx.putImageData(filteredData, 0, 0);
+}
+
+// File input event
+imageLoader.addEventListener('change', (e) => {
+  const file = e.target.files[0];
+  if (file) loadAndProcessImage(file);
+});
+
+// Apply filter button
+applyFilterButton.addEventListener('click', () => {
+  const filterType = filterSelect.value;
+  if (filterType) applyFilterToCanvas(filterType);
+});
